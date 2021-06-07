45b261d28adad88736347b62716e23be20bdb6ea
lab3000 <lukmaan@gmail.com>
2021-06-07 12:36:21 -0700

    get_flow_id.py works locally

--- local_tests/get_flow_id.py
+++ local_tests/get_flow_id.py
@@ -1,26 +1,35 @@
 import json
+import toml
 import os
 import urllib.parse
 import urllib.request
 from os.path import expanduser as eu
 
 prefect_settings = toml.load(eu('~/.prefect/client/https-api.prefect.io-graphql/settings.toml')) 
-api_token = prefect_settings['api_token'] 
+PREFECT__CLOUD__API = 'https://api.prefect.io/'
+PREFECT__CLOUD__AUTH_TOKEN = prefect_settings['api_token'] 
 
-query_ = '''
-query {
-    flow { 
-        name 
-        id
+def api_call():
+    query_ = '''
+    query {
+        flow { 
+            name 
+            id
+        }
     }
-}
-'''
+    '''
 
-data = json.dumps(dict(query=query_)).encode('utf-8')
+    data = json.dumps(dict(query=query_)).encode('utf-8')
 
-## prep the request
-req = urllib.request.Request(api_token, data=data)
-req.add_header("Content-Type", "application/json")
-req.add_header(
-    "Authorization", "Bearer {}".format(os.getenv("PREFECT__CLOUD__AUTH_TOKEN"))
-)
+    ## prep the request
+    req = urllib.request.Request(PREFECT__CLOUD__API, data=data)
+    req.add_header("Content-Type", "application/json")
+    req.add_header(
+        "Authorization", "Bearer {}".format(PREFECT__CLOUD__AUTH_TOKEN)
+    )
+
+    resp = urllib.request.urlopen(req)
+    return json.loads(resp.read().decode())
+
+if __name__=='__main__':
+    print(api_call())
